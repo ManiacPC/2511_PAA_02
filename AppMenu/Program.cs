cad59c345c03b04e92363e7424ca97f6b93ecaf8
Enrique Palacios <enriquepalaciosc@gmail.com>
2019-04-18 15:45:59 -0300

    Proyecto adaptado a patrón DAL - DTO

--- AppMenu/Program.cs
+++ AppMenu/Program.cs
@@ -1,16 +1,15 @@
-﻿using System;
+﻿using AppMenuModel.DAL;
+using AppMenuModel.DTO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//using AppMenu.DTO; // Opcion 1
 
 namespace AppMenu
 {
     class Program
     {
-        private Persona persona = new Persona();
-
         static void Main(string[] args)
         {
             while(Menu()) {};
@@ -37,22 +36,23 @@ namespace AppMenu
 
             string o = Console.ReadLine().Trim();
 
+           
             switch (o)
             {
                 case "1":
-                    Agregar();
+                    new PersonaDAL().Agregar();
                     // Hay que agregar
                     break;
                 case "2":
-                    Eliminar();
+                    new PersonaDAL().Eliminar();
                     // Hay que eliminar
                     break;
                 case "3":
-                    Listar();
+                    new PersonaDAL().Listar();
                     // Hay que listar
                     break;
                 case "4":
-                    Buscar();
+                    new PersonaDAL().Buscar();
                     // Hay que buscar
                     break;    
                 case "0":
@@ -67,90 +67,6 @@ namespace AppMenu
             return continuar;
         }
 
-        private static void Eliminar()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Buscar()
-        {
-            Console.Write("Ingrese un rut a buscar:");
-            string b = Console.ReadLine().Trim();
-
-            int i = Persona.Find(b);
-            string mensaje = 
-                (i >= 0 
-                ? $"Persona: {Persona.List()[i].Nombre} encontrada" 
-                : $"No encontrado ({b})"
-                );
-            Console.WriteLine(mensaje);
-            Console.ReadKey();
-            //if(i >= 0)
-            //{
-            //    Console.WriteLine($"Persona: {Persona.List()[i].Nombre} encontrada");
-            //}
-            //else
-            //{
-            //    Console.WriteLine($"No encotrado ({b})");
-            //}
-        }
-
-        private static void Listar()
-        {
-            List<Persona> personas = Persona.List();
-
-            foreach(Persona p in personas)
-            {
-                Console.WriteLine($"Rut: {p.Rut} || Nombre: {p.Nombre} || Edad: {p.Edad}");
-            }
-            Console.ReadKey();
-        }
-
-        private static void Agregar()
-        {
-            Console.WriteLine("-- Ingrese los datos de persona --");
-            Console.Write("Rut: ");
-            string rut = Console.ReadLine().Trim();
-
-            Console.Write("Nombre: ");
-            string nombre = Console.ReadLine().TrimStart().TrimEnd();
-
-            Console.Write("Edad: ");
-            int edad = 0;
-
-            // Forma 1
-            //int edad = Int32.Parse(Console.ReadLine().Trim());
-            //try
-            //{
-            //    edad = Convert.ToInt32(Console.ReadLine().Trim());
-            //}
-            //catch (Exception) {}
-
-            //Persona p = new Persona();
-            //p.Rut = rut;
-            //p.Nombre = nombre;
-            //p.Edad = edad;
-
-            // Forma 2
-            string r = Console.ReadLine().Trim();
-            if(!int.TryParse(r, out edad))  // TryParse -> bool
-            {
-                edad = -1;
-            }
-
-            if(!Persona.Add(
-                new Persona()
-                {
-                    Rut = rut,
-                    Nombre = nombre,
-                    Edad = edad
-                }))
-            {
-                Console.WriteLine("Ocurrió un error al ingresar persona");
-            }
-
-            Console.WriteLine($"{nombre} fue agregado exitosamente");
-            Console.ReadKey();
-        }
+        
     }
 }
--- AppMenuModel/DTO/Persona.cs
+++ AppMenuModel/DTO/Persona.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppMenu
+namespace AppMenuModel.DTO
 {
     public class Persona
     {
